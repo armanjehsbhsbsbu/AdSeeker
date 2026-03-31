2ad10ac854bdd36b613d6add9e28ba857b5bbcad
armanjehsbhsbsbu <armanpagranij2@gmail.com>
2026-03-31 18:45:00 +0100

    Add files via upload

---
+++ server (1).js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log('AdSeeker running on port ' + PORT);
+});
